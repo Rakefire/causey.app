5a9db362db7397556b499cffc239d926c2524243
Ricky Chilcott <ricky@missionmet.com>
2023-06-13 16:56:44 -0400

    Improve all the things

--- site/assets/js/dark-mode.js
+++ site/assets/js/dark-mode.js
@@ -3,26 +3,28 @@ var modeLabel = document.getElementById('js-mode-label');
 var mode = localStorage.getItem('mode');
 
 function changeMode() {
-	document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark');
 
-	if (document.body.classList.contains('dark')) {
-		modeLabel.innerText = 'Light Mode'
-		localStorage.setItem('mode', 'dark');
-	} else {
-		modeLabel.innerText = 'Dark Mode'
-		localStorage.setItem('mode', 'light');
-	}
+  if (document.body.classList.contains('dark')) {
+    modeLabel.innerText = 'Light Mode'
+    localStorage.setItem('mode', 'dark')
+    document.html.classList.remove("sl-theme-dark")
+  } else {
+    modeLabel.innerText = 'Dark Mode'
+    localStorage.setItem('mode', 'light')
+    document.html.classList.add("sl-theme-dark")
+  }
 }
 
 function checkMode() {
-	if (mode) {
-		if (mode == 'dark') {
-			document.body.classList.add('dark');
-			modeLabel.innerText = 'Light Mode';
-		} else {
-			document.body.classList.remove('dark');
-		}
-	}
+  if (mode) {
+    if (mode == 'dark') {
+      document.body.classList.add('dark');
+      modeLabel.innerText = 'Light Mode';
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
 }
 
 checkMode();
